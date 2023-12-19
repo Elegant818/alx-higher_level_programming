8fb785ba19c63784f6a96d829d2ae2713ad3af82
Elegant818 <giftpeteronyeche818@gmail.com>
2023-12-19 04:16:02 -0800

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+""" Square class, no module imported"""
+
+
+class Square:
+    """ Square class that defines a square """
+
+    def __init__(self, size=0):
+        """
+        initialize size value
+        args:
+        size (int): the size of square
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        calculate the area of the square
+        return: the area calculated
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        gets the value of size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the value of size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
